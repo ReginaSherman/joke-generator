c27db1b0307c57ebdd5b99cd8072b9f1c687e6ff
Regina Sherman <92479371+ReginaSherman@users.noreply.github.com>
2022-03-14 17:20:04 -0500

    Delete Desktop/capstone directory

--- Desktop/capstone/pages/api/s3-upload.js
+++
@@ -1,8 +0,0 @@
-import { APIRoute } from 'next-s3-upload';
-
-export default APIRoute.configure({
-  async key(req, filename) {
-    const user = await currentUser(req);
-    return `users/${user.id}/${filename}`;
-  },
-});
