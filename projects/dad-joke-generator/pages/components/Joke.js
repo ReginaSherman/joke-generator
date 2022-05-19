141da2b5aec31d339f3953fce24cf2690fb6f8f2
Reg-Sherman <reginaxsherman@gmail.com>
2022-05-19 13:18:36 -0500

    flexbox and css grid styling

--- projects/dad-joke-generator/pages/components/Joke.js
+++ projects/dad-joke-generator/pages/components/Joke.js
@@ -4,6 +4,7 @@ import styles from '../styles/Joke.module.css'
 export const Joke = () => {
     let [joke, setJoke] = useState(null)
     let [clicked, setClicked] = useState(false)
+    console.log(joke)
     const options = {
         method: 'GET',
         headers: {
@@ -11,23 +12,30 @@ export const Joke = () => {
             'X-RapidAPI-Key': process.env.NEXT_PUBLIC_API_KEY
         }
     }
-    
+
     const fetchData = () => {
         fetch(`https://dad-jokes.p.rapidapi.com/random/joke`, options)
             .then(response => response.json())
             .then(data => setJoke(data.body[0]))
             .catch(err => console.error(err));
-            setClicked(false)
+        setClicked(false)
 
     }
 
     return (
-        <div>
-            <h1 className={styles.text}>{joke?.setup}</h1>
-            {clicked === false ? null : <h2 className={styles.text}>{joke?.punchline}</h2>}
-            <button className={styles.button} onClick={() => setClicked(true)}>Punchline</button>
-            <button className={styles.button} onClick={fetchData}>New Joke</button>
+        <div className={styles.container}>
+            <div className={styles.textContainer}>
+                <h1 className={styles.text}>{joke?.setup}</h1>
+                {clicked === false ? null : <h2 className={styles.text}>{joke?.punchline}</h2>}
+            </div>
+            <div className={styles.buttonContainer}>
+                <button className={styles.button} onClick={() => setClicked(true)}>Punchline</button>
+                <button className={styles.button} onClick={fetchData}>New Joke</button>
+            </div>
         </div>
+
+
+
     );
 }
 
