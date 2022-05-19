8048c16351e3905dab15b328087b3c0748d1f4eb
Reg-Sherman <reginaxsherman@gmail.com>
2022-05-18 19:38:08 -0500

    buttons working, style tweaks

--- projects/dad-joke-generator/pages/components/Joke.js
+++ projects/dad-joke-generator/pages/components/Joke.js
@@ -1,28 +1,33 @@
-import React, {useState, useEffect} from 'react';
-
+import React, { useState, useEffect } from 'react';
+import styles from '../styles/Joke.module.css'
 
 export const Joke = () => {
-  let [joke, setJoke] = useState(null)
-  const options = {
-            method: 'GET',
-            headers: {
-                'X-RapidAPI-Host': process.env.NEXT_PUBLIC_HOST,
-                'X-RapidAPI-Key': process.env.NEXT_PUBLIC_API_KEY            }
+    let [joke, setJoke] = useState(null)
+    let [clicked, setClicked] = useState(false)
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Host': process.env.NEXT_PUBLIC_HOST,
+            'X-RapidAPI-Key': process.env.NEXT_PUBLIC_API_KEY
         }
-        useEffect(() => {
-                    fetch(`https://dad-jokes.p.rapidapi.com/random/joke`, options)
-                        .then(response => response.json())
-                        .then(data => setJoke(data.body[0]))
-                        .catch(err => console.error(err));
-            
-                }, [])
+    }
+    
+    const fetchData = () => {
+        fetch(`https://dad-jokes.p.rapidapi.com/random/joke`, options)
+            .then(response => response.json())
+            .then(data => setJoke(data.body[0]))
+            .catch(err => console.error(err));
+            setClicked(false)
 
-  return (
-    <div>
-        <h1>{joke?.setup}</h1>
-        <h2>{joke?.punchline}</h2>
+    }
 
-    </div>
-  );
+    return (
+        <div>
+            <h1 className={styles.text}>{joke?.setup}</h1>
+            {clicked === false ? null : <h2 className={styles.text}>{joke?.punchline}</h2>}
+            <button className={styles.button} onClick={() => setClicked(true)}>Punchline</button>
+            <button className={styles.button} onClick={fetchData}>New Joke</button>
+        </div>
+    );
 }
 
--- projects/dad-joke-generator/pages/index.js
+++ projects/dad-joke-generator/pages/index.js
@@ -1,12 +1,9 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import { Joke } from './components/Joke'
+import styles from './styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div>
-      <p1>Dad Joke Generator</p1>
-      <button>New Joke</button>
+    <div className={styles.container}>
       <Joke />
     </div>
   )
